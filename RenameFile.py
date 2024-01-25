0ae21ea90a54e09facaf3343493cedc439ee5317
SomeBody <nene.anantakul@gmail.com>
2024-01-25 15:29:18 +0700

    Update1

---
+++ RenameFile.py
@@ -0,0 +1,19 @@
+import os 
+location = "D:/WorkCodePython/Kru_Oud/Batch_Rename/Image/"
+dir_list = os.listdir(location)
+
+for file_name in dir_list : 
+    name,ext = os.path.splitext(file_name)
+    
+
+
+    
+   
+
+
+    
+
+
+
+
+
