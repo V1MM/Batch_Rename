5b5048a01ec068cafcc7430e45c58eec0c90a472
SomeBody <nene.anantakul@gmail.com>
2024-01-27 16:09:32 +0700

    Update 2 No func

--- RenameFile.py
+++ RenameFile.py
@@ -1,19 +1,17 @@
 import os 
-location = "D:/WorkCodePython/Kru_Oud/Batch_Rename/Image/"
+location = os.chdir("D:/6610301012Work/BigYukPython/Batch_Rename/Image")
 dir_list = os.listdir(location)
+counter = 1
+print(": WELCOME TO BATCH RENAMING TOOL :")
+file_ex = input("please select file extension (.jpg , .png etc) = ")
 
 for file_name in dir_list : 
-    name,ext = os.path.splitext(file_name)
+    new_file_name = "{:03d}.{}".format(counter,file_ex)
+    os.rename(file_name , new_file_name)
+    counter += 1
+
+# if __name__ == "__main__" :
+
+
+
     
-
-
-    
-   
-
-
-    
-
-
-
-
-
