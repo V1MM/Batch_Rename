0a673708668f378df0e251940bf8859d5893e052
SomeBody <nene.anantakul@gmail.com>
2024-01-27 19:54:18 +0700

    Fix #1

--- RenameFile.py
+++ RenameFile.py
@@ -1,17 +1,22 @@
 import os 
-location = os.chdir("D:/6610301012Work/BigYukPython/Batch_Rename/Image")
-dir_list = os.listdir(location)
-counter = 1
-print(": WELCOME TO BATCH RENAMING TOOL :")
-file_ex = input("please select file extension (.jpg , .png etc) = ")
+def Batch_Rename() :
+ location = os.chdir("D:/6610301012Work/BigYukPython/Batch_Rename/Image")
+ dir_list = os.listdir(location)
+ print(": WELCOME TO BATCH RENAMING TOOL :")
+ file_ex = input("please select file extension (.jpg , .png etc) = ")
+ i = 1
 
-for file_name in dir_list : 
-    new_file_name = "{:03d}.{}".format(counter,file_ex)
+ for file_name in dir_list : 
+   name,ext = os.path.splitext(file_name)
+   if ext == file_ex :  
+    new_file_name = "{:03d}{}".format(i , ext)
     os.rename(file_name , new_file_name)
-    counter += 1
+    print(new_file_name)
+    i += 1
 
-# if __name__ == "__main__" :
+if __name__ == "__main__" :
+  Batch_Rename() 
 
 
 
-    
+        
